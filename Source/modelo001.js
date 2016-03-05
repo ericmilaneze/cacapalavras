7826cfbde08a25a1dfd761b82eb4d6f02f660dd4
Eric Milaneze <eric@milaneze.com.br>
2016-03-05 17:10:39 -0300

    Modelo

--- Source/modelo001.js
+++ Source/modelo001.js
@@ -1,6 +1,5 @@
 (function() {
     var configuracoesDeJogoPadrao = {"possuiBordas":true,"canvasWidth":250,"canvasHeight":250,"config":{}};
-    var sectionsDeJogo = document.getElementsByClassName("sectionJogo");
 
     var sectionJogoMouseOver = function() {
         var sectionAtual = this;
@@ -8,8 +7,8 @@
                 
         var divReconfigurar = sectionAtual.querySelector("div.reconfigurar");
         divReconfigurar.classList.remove("invisivel");
-        divReconfigurar.style.top = (canvasJogo.height - 15) + "px";
-        divReconfigurar.style.left = (canvasJogo.width + (canvasJogo.getBoundingClientRect().left - sectionAtual.getBoundingClientRect().left) - divReconfigurar.clientWidth - 15) + "px";
+        divReconfigurar.style.top = (canvasJogo.height + (canvasJogo.getBoundingClientRect().top - sectionAtual.getBoundingClientRect().top) - 20) + "px";
+        divReconfigurar.style.left = (canvasJogo.width + (canvasJogo.getBoundingClientRect().left - sectionAtual.getBoundingClientRect().left) - divReconfigurar.clientWidth) + "px";
         
         sectionAtual.classList.add("mouseover");
     };
@@ -37,30 +36,106 @@
         
         sectionAtual.classList.remove("mouseover");
     };
+    
+    var insertAfter = function(newElement, targetElement) {
+        var parent = targetElement.parentNode;
 
-    for (var i = 0; i < sectionsDeJogo.length; i++) {
-        var sectionAtual = sectionsDeJogo[i];
+        if(parent.lastchild === targetElement) 
+            parent.appendChild(newElement);
+        else
+            parent.insertBefore(newElement, targetElement.nextSibling);
+    };
+    
+    var criarNovaSecton = function() {
+        var sectionAtual = this.parentElement;
+
+        sectionAtual.innerHTML = "<div class=\"linha\"><div class=\"textoAcima\" style=\"display: none;\"></div><canvas class=\"jogo\" width=\"470\" height=\"456\"></canvas><div class=\"palavrasDoJogo\" style=\"display: none;\">Salma<br>Minas<br>Gerais<br>Marcia<br>Mirian<br>Ubirajara<br></div><div class=\"clear\"></div><div class=\"reconfigurar invisivel\" style=\"top: 444px; left: 445px;\"><a href=\"javascript:void(0)\"><img src=\"./imagens/edit.png\"></a></div></div><input type=\"text\" class=\"configJogo\"><input type=\"button\" value=\"Ok\" class=\"botaoOk\">";
         
-        if(!sectionAtual.classList.contains("naoAdicionado")) {
-            desenharJogo(sectionAtual, configuracoesDeJogoPadrao);
+        sectionAtual.classList.remove("naoAdicionado");
+        sectionAtual.classList.add("sectionJogo");
+        
+        sectionAtual.style.width = "";
+        sectionAtual.style.height = "";
+        sectionAtual.style.lineHeight = "";
+        
+        init();
+    };
+    
+    var criarNovaSectionVazia = function(sectionAtual, elementoAnterior, isPrimeiroDaLinha) {
+        var height = (sectionAtual.getBoundingClientRect().bottom - sectionAtual.getBoundingClientRect().top);
+        
+        var newInsideLink = document.createElement("a");
+        newInsideLink.attributes["href"] = "javascript:void(0)";
+        newInsideLink.innerText = "Adicionar um novo.";
+        newInsideLink.addEventListener("click", criarNovaSecton);
+        
+        var newSection = document.createElement("section");
+        newSection.classList.add("sectionJogo");
+        newSection.classList.add("naoAdicionado");
+        newSection.appendChild(newInsideLink);
+        newSection.style.width = sectionAtual.clientWidth + "px";
+        newSection.style.height = height + "px";
+        newSection.style.lineHeight = height + "px";
+        
+        if(isPrimeiroDaLinha)
+            newSection.classList.add("primeiroDaLinha");
+        
+        insertAfter(newSection, elementoAnterior);
+        
+        return newSection;
+    };
+    
+    var criarDivPularLinha = function(sectionRecemAdicionada) {
+        var div = document.createElement("div");
+        div.classList.add("clear");
+        
+        insertAfter(div, sectionRecemAdicionada);
+        
+        return div;
+    };
+    
+    var botaoOkClick = function() {
+        var sectionAtual = this.parentElement;
+        var txtConfig = sectionAtual.querySelector("input.configJogo");
 
-            sectionAtual.querySelector("input.botaoOk").addEventListener("click", function() {
-                var sectionAtual = this.parentElement;
-                var txtConfig = sectionAtual.querySelector("input.configJogo");
-
-                desenharJogo(sectionAtual, JSON.parse(txtConfig.value));
-                
-                txtConfig.style.display = "none";
-                this.style.display = "none";
-                
-                sectionAtual.addEventListener("mouseover", sectionJogoMouseOver);
-                sectionAtual.addEventListener("mouseout", sectionJogoMouseOut);
-                
-                sectionAtual.querySelector("div.reconfigurar > a").addEventListener("click", reconfigurarLinkClick);
-            });
-        }
-        else {
+        desenharJogo(sectionAtual, JSON.parse(txtConfig.value));
+        
+        txtConfig.style.display = "none";
+        this.style.display = "none";
+        
+        sectionAtual.addEventListener("mouseover", sectionJogoMouseOver);
+        sectionAtual.addEventListener("mouseout", sectionJogoMouseOut);
+        
+        sectionAtual.querySelector("div.reconfigurar > a").addEventListener("click", reconfigurarLinkClick);
+        
+        if(!sectionAtual.classList.contains("criado")) {
+            var sectionRecemAdicionada = criarNovaSectionVazia(sectionAtual, sectionAtual, false);
             
+            if(sectionAtual.classList.contains("primeiroDaLinha")) {
+                var divPularLinha = criarDivPularLinha(sectionRecemAdicionada);
+                
+                criarNovaSectionVazia(sectionAtual, divPularLinha, true);
+            }
+        
+            sectionAtual.classList.add("criado");
+        };
+    };
+    
+    var init = function() {
+        var sectionsDeJogo = document.getElementsByClassName("sectionJogo");
+        
+        for (var i = 0; i < sectionsDeJogo.length; i++) {
+            var sectionAtual = sectionsDeJogo[i];
+            
+            if(!sectionAtual.classList.contains("naoAdicionado") && !sectionAtual.classList.contains("desenhado")) {
+                desenharJogo(sectionAtual, configuracoesDeJogoPadrao);
+
+                sectionAtual.querySelector("input.botaoOk").addEventListener("click", botaoOkClick);
+                
+                sectionAtual.classList.add("desenhado");
+            }
         }
-    }    
+    }
+    
+    init();
 })();
